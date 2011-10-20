fd7347360396cbba538e06dce3ba44ac3b6b6fc2
wangshijun2010@gmail.com <wangshijun2010@gmail.com@b4babe0d-84df-d1e8-5375-62ff76a3aef0>
2011-10-20 01:56:36 +0000

    Init: 将Etao商品下架助手放到GoogleCode的Project Hosting中

---
+++ trunk/js/assistant.inject.js
@@ -0,0 +1,48 @@
+// 提取NID并且创建复选框
+var items = $('li.entry');
+
+//console.log('SRP items found: ' + items.length);
+
+$.each(items, function (i, item) {
+	// 提取NID
+	$(this).css({position: 'relative'});
+	var href = $('a:has(img):first', this).attr('href'),
+		title = $('h3 a', this).attr('title'),
+		nid = false;
+	if (href != '') {
+		var regex_id = /\?id=\d+/gi;
+		var regex_nid = /&nid=\d+/gi;
+
+		var matches_id = href.match(regex_id);
+		var matches_nid = href.match(regex_nid);
+
+		if (matches_id) {
+			nid = matches_id[0].substring(4, matches_id[0].length);
+		} else if (matches_nid) {
+			nid = matches_nid[0].substring(5, matches_nid[0].length);
+		}
+	}
+
+	// 创建复选框
+	if (nid) {
+		//console.log('#' + i + ', NID: ' + nid + ', TITLE: ' + title);
+		var checkbox = $('<input type="checkbox" class="injected" title="单击将该商品加入垃圾箱" style="position: absolute; left: 0px; top: 0px; width: 5em; height: 6em;">');
+		$('a:has(img):first', this).append(checkbox.click(function () {
+			var request = {nid: nid, url: href, title: title, status: this.checked};
+			chrome.extension.sendRequest(request, function(response) {
+				console.log('status:' + response.status + ', code: ' + response.code);
+			});
+		}));
+	}
+
+});
+
+// 每次添加完毕之后, 取消所有复选框
+chrome.extension.onRequest.addListener(
+	function(request, sender, sendResponse) {
+		if (request.completed) {
+			$('input.injected').attr('checked', false);
+		}
+		sendResponse({});
+	}
+);
