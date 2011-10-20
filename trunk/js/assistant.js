4b472ef8e35c4f2e9f31cf06eacff71b80a9359f
wangshijun2010@gmail.com <wangshijun2010@gmail.com@b4babe0d-84df-d1e8-5375-62ff76a3aef0>
2011-10-20 07:44:21 +0000

    feature: 完成了基本的偏好设置页面

--- trunk/js/assistant.js
+++ trunk/js/assistant.js
@@ -1,1 +1,40 @@
-var assistant = { };
+var assistant = {
+	// 系统通知消息
+	notify: function(message, type, callback) {
+		var callback = callback || function () {};
+
+		var popup = $('<div class="message"></div>')
+			.text(message).addClass(type).hide()
+			.appendTo(document.body);
+
+		popup
+			.css({ left: ($(window).width() - popup.width() ) / 2 + 'px' })
+			.fadeIn('fast');
+
+		setTimeout(function () {
+			popup.fadeOut('fast', callback);
+		}, 3000);
+	},
+
+	// 读取偏好
+	getOption: function (key) {
+		var value = window.localStorage.getItem(key);
+		if (value === null) {
+			value = assistant.defaults[key] ? assistant.defaults[key] : null;
+		}
+		return value;
+	},
+
+	// 设置偏好
+	setOption: function (key, value) {
+		return window.localStorage.setItem(key, value);
+	},
+
+	// 默认选项
+	defaults: {
+		'selector_entry': 'li.entry',
+		'selector_link': 'a:has(img):first',
+		'selector_title': 'h3 a',
+		'server_url': 'http://eharmony.taobao.com/eharmony/'
+	}
+};
