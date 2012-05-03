a951a15ad2aff0efac914581b97abd7efbbda533
wangshijun2010 <wangshijun2010@gmail.com>
2012-05-03 05:42:02 +0000

    tools-user-agent-switcher code formatting

--- tools-user-agent-switcher/spoofer_cs.js
+++ tools-user-agent-switcher/spoofer_cs.js
@@ -1,28 +1,31 @@
-chrome.extension.sendRequest({action: "blacklist", url: document.location.href}, function(response) {
-  if (response.ua_string && response.ua_string != "") {
-    document.addEventListener("beforeload", function(event) {
-      var new_nav = new function() {};
-      var x;
-      for (x in navigator) {
-        new_nav[x] = navigator[x];
-      }
-      new_nav.userAgent = response.user_agent;
-      new_nav.vendor = response.vendor;
-      window.navigator = new_nav;
-    }, true);
-    
-    var script = document.createElement("script");
-    script.type = "text/javascript";
-    script.innerText += 'var new_nav = new function() {};';
-    script.innerText += 'var x;';
-    script.innerText += 'for (x in navigator) {';
-    script.innerText += 'eval("new_nav." + x + " = navigator." + x + ";");';
-    script.innerText += '}';
-    script.innerText += 'new_nav.userAgent = "' + response.ua_string + '";';
-    script.innerText += 'new_nav.vendor = "' + response.vendor + '";';
-    script.innerText += 'window.navigator = new_nav;';
-    document.documentElement.insertBefore(script);
-    
-  }
-});
+chrome.extension.sendRequest({
+	action: "blacklist",
+	url: document.location.href
+}, function (response) {
+	if (response.ua_string && response.ua_string != "") {
+		document.addEventListener("beforeload", function (event) {
+			var new_nav = new
+			function () {};
+			var x;
+			for (x in navigator) {
+				new_nav[x] = navigator[x];
+			}
+			new_nav.userAgent = response.user_agent;
+			new_nav.vendor = response.vendor;
+			window.navigator = new_nav;
+		}, true);
 
+		var script = document.createElement("script");
+		script.type = "text/javascript";
+		script.innerText += 'var new_nav = new function() {};';
+		script.innerText += 'var x;';
+		script.innerText += 'for (x in navigator) {';
+		script.innerText += 'eval("new_nav." + x + " = navigator." + x + ";");';
+		script.innerText += '}';
+		script.innerText += 'new_nav.userAgent = "' + response.ua_string + '";';
+		script.innerText += 'new_nav.vendor = "' + response.vendor + '";';
+		script.innerText += 'window.navigator = new_nav;';
+		document.documentElement.insertBefore(script);
+
+	}
+});
